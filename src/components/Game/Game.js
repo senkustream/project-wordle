49ee06e2f88469010869e5d771d906e108c59bc7
Kresna Satya <kresnasatya72@gmail.com>
2024-10-16 10:06:59 +0800

    finish exercise 2: GuessResults

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,7 @@ import { sample } from '../../utils';
 import { WORDS } from '../../data';
 
 import GuessInput from '../GuessInput';
+import GuessResults from '../GuessResults';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -11,8 +12,15 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
+  const [guesses, setGuesses] = React.useState([]);
+  function handleGuessInput(value) {
+    const updatedGuesses = [...guesses];
+    updatedGuesses.push(value);
+    setGuesses(updatedGuesses);
+  }
   return <>
-    <GuessInput/>
+    <GuessResults guesses={guesses}/>
+    <GuessInput handleGuessInput={handleGuessInput} />
   </>;
 }
 
--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,25 +1,25 @@
 import React from "react";
 
-function GuessInput() {
-    const [guess, setGuess] = React.useState('');
+function GuessInput({ handleGuessInput }) {
+    const [value, setValue] = React.useState('');
 
     return (
         <form className="guess-input-wrapper"
             onSubmit={(event) => {
                 event.preventDefault();
-                console.log({ guess });
-                setGuess('');
+                handleGuessInput(value);
+                setValue('');
             }}>
             <label htmlFor="guess-input">Enter guess:</label>
             <input id="guess-input" type="text" 
-                value={guess}
+                value={value}
                 minLength={5}
                 maxLength={5}
                 pattern="[a-zA-Z]{5}"
                 title="5 letter word"
                 onChange={(event) => {
                     const value = event.target.value;
-                    setGuess(value.toUpperCase());
+                    setValue(value.toUpperCase());
                 }}/>
         </form>
     )
